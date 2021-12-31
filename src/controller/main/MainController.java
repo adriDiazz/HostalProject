9cee1e71990ad76885a44652160dc8ace657b081
adriDiazz <adriandiazmanzanares9@gmail.com>
2021-12-31 15:37:10 +0100

    First view

---
+++ src/controller/main/MainController.java
@@ -0,0 +1,13 @@
+package controller.main;
+
+import javafx.stage.Stage;
+
+public class MainController {
+	
+	private Stage mainWindow;
+	
+	public void setMainWindow(Stage mainWindow) {
+		this.mainWindow = mainWindow;
+	}
+
+}
